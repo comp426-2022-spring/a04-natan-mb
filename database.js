5549b0680d0b6271c6dc2e6b6cc25f7b9e8d3fc0
natan-mb <78055688+natan-mb@users.noreply.github.com>
2022-04-02 18:58:41 -0400

    defined all endpoints

--- database.js
+++ database.js
@@ -8,9 +8,7 @@ const Database = require('better-sqlite3');
 const db = new Database('log.db')
 
 // Is the database initialized or do we need to initialize it?
-const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`
-    );
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`);
 
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
@@ -18,18 +16,15 @@ let row = stmt.get();
 // Check if there is a table. If row is undefined then no table exists.
 if (row === undefined) {
 // Echo information about what you are doing to the console.
-    console.log('Your database appears to be empty. I will initialize it now.');
+    console.log('initializing database...');
 // Set a const that will contain your SQL commands to initialize the database.
-    const sqlInit = `
-        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, username TEXT, password TEXT );
-        INSERT INTO userinfo (username, password) VALUES ('user1','supersecurepassword'),('test','anotherpassword');
-    `;
+    const sqlInit = 'CREATE TABLE accesslog ( remoteaddr TEXT, remoteuser BLOB, time INTEGER PRIMARY KEY, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, secure INTEGER, status INTEGER, referer TEXT, useragent TEXT )'
 
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
 
 // Echo information about what we just did to the console.
-    console.log('Your database has been initialized with a new table and two entries containing a username and password.');
+    console.log('database has been initialized');
 
 } else {
 // Since the database already exists, echo that to the console.
--- server.js
+++ server.js
@@ -3,23 +3,24 @@
 const args = require('minimist')(process.argv.slice(2))
 
 if (args.help) {
-    console.log(
-        `server.js [options] 
+    console.log(`
+        server.js [options] 
         
         --port  Set the port number for the server to listen on. Must be an integer
-        between 1 and 65535.
+                between 1 and 65535.
 
-        --debug  If set to 'true', creates endpoints /app/log/access/ which returns
-        a JSON access log from the database and /app/error which throws 
-        an error with the message "Error test successful." Defaults to 
-        'false'.
+        --debug If set to 'true', creates endpoints /app/log/access/ which returns
+                a JSON access log from the database and /app/error which throws 
+                an error with the message "Error test successful." Defaults to 
+                'false'.
 
         --log   If set to false, no log files are written. Defaults to true.
-        Logs are always written to database.
+                Logs are always written to database.
 
-        --help  Return this message and exit.`
-    )
-    exit(0)
+        --help  Return this message and exit.
+    `)
+
+    process.exit()
 }
 
 const call = args.call
@@ -32,8 +33,44 @@ const { exit } = require('process')
 const db = require('./database.js')
 
 var app = express()
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+const logEnabled = (args.log == undefined ? true : args.log)
+const debugEnabled = (args.debug == undefined ? false : args.debug)
+
+// Middleware function
+const logger = function(req, res, next) {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: (req.user),
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        secure: (req.secure ? 1 : 0),
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    }
+
+    //console.log(typeof logdata.remoteuser)
+
+    const stmt = db.prepare('INSERT INTO accesslog VALUES (@remoteaddr, @remoteuser, @time, @method, @url, @protocol, @httpversion, @secure, @status, @referer, @useragent)')
+
+    const info = stmt.run(logdata)
+
+    // const stmt = db.prepare('INSERT INTO accesslog (secure) VALUES (?)')
+    // const info = stmt.run(logdata.secure)
+    //res.status(200).json(info)
+    next()
+}
+
+if (logEnabled) {
+    app.use(logger)
+}
+
 
 // Start an app server
 const server = app.listen(port, () => {
@@ -50,6 +87,24 @@ app.get('/app/', (req, res) => {
     res.statusMessage = 'OK';
     res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
     res.end(res.statusCode + ' ' + res.statusMessage);
+});
+
+
+
+app.get('/app/types/', (req, res) => {
+    console.log(typeof req.ip);
+    console.log(req.user);
+    console.log(Date.now());
+    console.log(req.method);
+    console.log(typeof req.url);
+    console.log(typeof req.protocol);
+    console.log(typeof req.httpVersion);
+    console.log(typeof req.secure);
+    console.log(typeof res.statusCode);
+    console.log(req.headers['referer']);
+    console.log(typeof req.headers['user-agent']);
+
+    res.sendStatus(200)
 });
 
 
@@ -126,22 +181,25 @@ app.get('/app/flip/call/tails', (req, res) => {
 });
 
 
-// Debugging endpoints
 
-if (args.debug == true) {
+// Debugging endpoints
+if (debugEnabled) {
 
     app.get('/app/log/access/',  (req, res) => {
-        const stmt = db.prepare('SELECT * FROM accesslog')
-        const info = stmt.run()
-        res.status(200).send(info)
+        try {
+            const stmt = db.prepare('SELECT * FROM accesslog').all()
+            res.status(200).json(stmt)
+        } catch {
+            console.error('could not retrieve database')
+        }
     });
 
-
+    
     app.get('/app/error',  (req, res) => {
-        
+        throw new Error('Error test successful.')
     });
-}
 
+}
 
 
 // Default response for any other request
